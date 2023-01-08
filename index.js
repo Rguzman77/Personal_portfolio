540123f9b2983923f95ac7308575ee693646e977
Emmanuel <116069729+Rguzman77@users.noreply.github.com>
2023-01-08 21:09:54 +0100

    added bootstrap carousel and nav bar

---
+++ index.js
@@ -0,0 +1,7 @@
+const aboutme = document.querySelector(".about-me");
+const projects= document.querySelector(".projects-container");
+const clients = document.querySelector(".clients-container");
+const cvcontainer = document.querySelector(".cv-container");
+const contact = document.querySelector(".contact-container");
+
+
